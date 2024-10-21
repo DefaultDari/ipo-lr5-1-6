023b2999829371acbe128e9311ca441031db8743
Даниил Луценко <110895420+DefaultDari@users.noreply.github.com>
2024-10-21 15:13:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-with open('C:\\Users\\agjjj\Desktop\\text.txt', 'r') as text_txt: # Открываем исходный файл text.txt для чтения
-    with open('C:\\Users\\agjjj\Desktop\\output.txt', 'w') as output_txt: # Открываем файл output.txt для записи, а если его нет, он будет создан
-        line_number = 1 # Инициализируем переменную для нумерации строк, начиная с 1
-        for line in text_txt: # Проходим по каждой строке в файле text.txt            
-            number = f"{line_number}: {line}" # Создаем строку с номером строки и текстом строки            
-            output_txt.write(number) # Записываем эту строку в файл output.txt
-            line_number += 1 # Увеличиваем номер строки на 1 для следующей итерации
+file1 = open('D:\\profiles\\LutsenD_81\\Desktop\\text.txt', 'r') # Открываем исходный файл text.txt для чтения
+file2 = open('D:\\profiles\\LutsenD_81\\Desktop\\output.txt', 'w')  # Открываем файл output.txt для записи, а если его нет, он будет создан
+line_number = 1 # Инициализируем переменную для нумерации строк, начиная с 1
+for line in file1: # Проходим по каждой строке в файле text.txt            
+    number = f"{line_number}: {line}" # Создаем строку с номером строки и текстом строки            
+    file2.write(number) # Записываем эту строку в файл output.txt
+    line_number += 1 # Увеличиваем номер строки на 1 для следующей итерации
