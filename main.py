4049386ae74a263ee9c55f2c2853611e7d16ea59
Даниил Луценко <110895420+DefaultDari@users.noreply.github.com>
2024-11-17 16:10:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-file1 = open('D:\\profiles\\LutsenD_81\\Desktop\\text.txt', 'r') # Открываем исходный файл text.txt для чтения
-file2 = open('D:\\profiles\\LutsenD_81\\Desktop\\output.txt', 'w')  # Открываем файл output.txt для записи, а если его нет, он будет создан
+file1 = open("text.txt",'r', encoding="utf-8") # Открываем исходный файл text.txt для чтения
+file2 = open("output.txt",'w', encoding="utf-8")  # Открываем файл output.txt для записи, а если его нет, он будет создан
 line_number = 1 # Инициализируем переменную для нумерации строк, начиная с 1
 for line in file1: # Проходим по каждой строке в файле text.txt            
     number = f"{line_number}: {line}" # Создаем строку с номером строки и текстом строки            
